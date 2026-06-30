8c6487d41e97f8f88b3ebcbbe357c7128d5485ad
Gianluca Boiano <491117+M0Rf30@users.noreply.github.com>
2026-07-01 01:16:08 +0200

    fix: region 401, auto image lookup, WireGuard relay, remote state, hardened CI

--- oci-stack-module/outputs.tf
+++ oci-stack-module/outputs.tf
@@ -1,29 +1,29 @@
 output "public-ip-x86_64-instances" {
   description = "The public IP addresses assigned to the E2 instances"
-  value = oci_core_instance.vm_instance_x86_64.*.public_ip
+  value       = oci_core_instance.vm_instance_x86_64.*.public_ip
 }
 
 output "private-ip-x86_64-instances" {
   description = "The private IP addresses assigned to the E2 instances"
-  value = oci_core_instance.vm_instance_x86_64.*.private_ip
+  value       = oci_core_instance.vm_instance_x86_64.*.private_ip
 }
 
 output "instance-id-x86_64-instances" {
   description = "The OCID of the E2 instances"
-  value = oci_core_instance.vm_instance_x86_64.*.id
+  value       = oci_core_instance.vm_instance_x86_64.*.id
 }
 
 output "public-ip-ampere-instance" {
   description = "The public IP addresse assigned to the A2 instance"
-  value = oci_core_instance.vm_instance_ampere.public_ip
+  value       = oci_core_instance.vm_instance_ampere.public_ip
 }
 
 output "private-ip-ampere-instance" {
   description = "The private IP addresse assigned to the A2 instance"
-  value = oci_core_instance.vm_instance_ampere.private_ip
+  value       = oci_core_instance.vm_instance_ampere.private_ip
 }
 
 output "instance-id-ampere-instance" {
   description = "The OCID of the A1 instance"
-  value = oci_core_instance.vm_instance_ampere.id
+  value       = oci_core_instance.vm_instance_ampere.id
 }
